77850311c819f4b60a415ed47e203afef68696a5
rhymarscsu <markhamr1@southernct.edu>
2025-11-16 14:46:14 -0500

    Updated db.js for db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: process.env.DB_HOST || '34.26.213.135',
     user: process.env.DB_USER || 'sanj',
-    password: 'your_db_password_here',
+    password: 'Hubconn-3'
     database: process.env.DB_NAME || 'hubconnect',
     waitForConnections: true,
     connectionLimit: 10,
